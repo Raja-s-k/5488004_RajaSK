8924666cf0006db35a2df28c4b4d08e777fb2ede
Raja-s-k <rajask42042@gmail.com>
2025-08-08 09:58:00 +0530

    Added hackerrank codes

---
+++ HackerRank/Week1/Lonely_integer.c
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    lone = 0
+    
+    for i in a:
+        b = a.count(i)
+        if b==1:
+            lone = i
+            break
+            
+    return lone
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
